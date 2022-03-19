bdf659817f082c52c3c7902681b6c5e29bc1f7ed
vishmaurya <98794654+vishmaurya@users.noreply.github.com>
2022-03-20 01:17:07 +0530

    Project Serializer and Views [CREATE]

--- Project/views.py
+++ Project/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import ProjectSerializer, TagSerializer, TaskSerializer, SubTaskSerializer
+from .models import Project, Tag, Task, SubTask
+
+
+# ALL PROJECT VIEWS
+class ProjectView(APIView):
+    def get(self, request):
+        project = Project.objects.all()
+        response = ProjectSerializer(project, many=True)
+        return Response({"data": response.data}, status=200)
+
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+
+# PROJECT BY ID
+class SpecificProjectView(APIView):
+    def get(self, req, id):
+        project = Project.objects.get(id=id)
+        res = ProjectSerializer(project)
+        return Response({"Data": res.data}, status=200)
